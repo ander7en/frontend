28096f39bf9b010c5e15f6fec0cde3ae884d7c58
Andrii Khrystian <andrii.khrystian@gmail.com>
2016-11-10 23:45:22 +0200

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendTaxiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the frontendTaxiApp
+ */
+angular.module('frontendTaxiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
