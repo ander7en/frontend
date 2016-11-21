6ed5ef785617425354f68c656778c6e27ace2cd8
Demur Nodia <demur.nodia@gmail.com>
2016-11-22 01:37:25 +0200

    Add controller tests

---
+++ test/spec/controllers/map.spec.js
@@ -0,0 +1,73 @@
+describe('Unit: Map', function () {
+
+  // Define global references for injections.
+  var ctrl;
+
+  beforeEach(module('frontendTaxiApp'));
+
+  beforeEach(inject(function ($controller) {
+
+    // Instantiate the controller with an object of the dependencies
+    ctrl = $controller('MapCtrl', {
+      NgMap: {
+        getMap: function () {
+          return {
+            then: function (map) {
+              return map;
+            }
+          }
+        }
+      }
+    });
+
+    ctrl.getPlace = function () {
+      return {geometry: {location: 11}}
+    };
+
+    ctrl.map = {
+      setCenter: function (center) {
+
+      }
+    };
+  }));
+
+  describe('MapCtrl', function () {
+    // Test some basic expectations about the controller
+
+    it('should have pickupPlaceChanged defined', function () {
+      expect(ctrl.pickupPlaceChanged).toBeDefined()
+    });
+
+    it('should have destinationPlaceChanged defined', function () {
+      expect(ctrl.destinationPlaceChanged).toBeDefined()
+    });
+
+    it('should have current location onCurrentLocationDetected defined', function () {
+      expect(ctrl.onCurrentLocationDetected).toBeDefined()
+    });
+
+    it('should have updateRouteInfo defined', function () {
+      expect(ctrl.updateRouteInfo).toBeDefined()
+    });
+
+    it('should have distance duration pickupLocation destinationLocation undefined', function () {
+      expect(ctrl.distance).toBeUndefined();
+      expect(ctrl.duration).toBeUndefined();
+      expect(ctrl.pickupLocation).toBeUndefined();
+      expect(ctrl.destinationLocation).toBeUndefined();
+    });
+
+    it('should have pickupLocation defined after change pickup place', function () {
+      ctrl.pickupPlaceChanged();
+      expect(ctrl.pickupLocation).toBeDefined();
+      expect(ctrl.pickupLocation).toBe(11);
+    });
+
+    it('should have destinationLocation defined after change destination place', function () {
+      ctrl.destinationPlaceChanged();
+      expect(ctrl.destinationLocation).toBeDefined();
+      expect(ctrl.destinationLocation).toBe(11);
+    });
+
+  });
+});
