380446c80e92f38e0ef3e53364b80928af31129a
Andrii Khrystian <andrii.khrystian@gmail.com>
2016-12-13 08:13:51 +0200

    error message for user

--- app/scripts/services/bookingservice.js
+++ app/scripts/services/bookingservice.js
@@ -61,5 +61,12 @@ function BookingService($http, $rootScope, ENV, localStorageService, PusherFacto
       $rootScope.$apply();
 
     });
+    channel.bind('error', function (data) {
+      NotificationService.addAlert({
+        type: 'danger',
+        msg: data.message
+      });
+      $rootScope.$apply();
+    });
   }
 }
